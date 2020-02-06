09959cec4988d90f39b17af5232ba38f6546fc0e
Efrench621 <57417224+Efrench621@users.noreply.github.com>
2020-02-06 16:30:26 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,36 @@
+console.log('hello');
+
+//alert('hello this is Evan');
+
+// How to write a comment inline
+
+// Variables
+var b = 'gorillaz';
+console.log(b);
+
+var someNumber = 32;
+// console.log(someNumber);
+
+
+
+// var age = prompt('What is your age?');
+
+// document.getElementById('someText').innerHTML = age;
+
+// Numbers in Javascript
+var num1 = 10;
+
+// Increment num1 by 1
+num1++;
+
+// Decrement num1 by 1
+num1--;
+console.log(num1);
+
+// Divide, multiply *, remainder %
+console.log(num1 / 6);
+
+//increment/decrement by 10
+num1 += 10;
+console.log(num1);
+
