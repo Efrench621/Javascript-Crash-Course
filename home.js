18f3d8d0959c3056d7aae9131aa2afa43d2b54f1
Evan <evan_tyw@yahoo.com>
2020-02-18 23:47:36 -0700

    Progress Update #1

--- home.js
+++ home.js
@@ -63,8 +63,8 @@ function greeting(yourName){
     console.log(result);
 }
 
-// var name = prompt('what is your name?');
-greeting(name);
+/* var name = prompt('what is your name?');
+greeting(name);*/
 
 // How do argurments work in functions?
 // How do we add 2 numbers together in a function?
@@ -100,3 +100,76 @@ let groceries = ['apple', 'banana', 'oranges']; // array
 let random; // undefined
 let nothing = null; // value null
 
+// Strings in Javascript (common methods)
+let fruit = 'banana,apple,orange,blackberry';
+let moreFruits = 'banana/apple';
+console.log(moreFruits);      // new line
+
+console.log(fruit.length);
+console.log(fruit.indexOf('nan'));
+console.log(fruit.slice(2, 6));
+console.log(fruit.replace('ban', '123'));
+console.log(fruit.toUpperCase(fruit));
+console.log(fruit.toLowerCase(fruit));
+console.log(fruit[2]);
+console.log(fruit.charAt(2));
+console.log(fruit.split(','));        // split by a comma
+console.log(fruit.split(''));         // split by character
+
+// Array
+let fruits = ['banana', 'apple', 'orange', 'pineapples'];
+fruits = new Array('banana', 'apple', 'orange', 'pineapples');
+
+console.log(fruits[2]);     // access value at index 2nd
+
+fruits[0] = 'pear';
+console.log(fruits);
+
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[1]);
+}
+
+// array common methods
+console.log('to string', fruits.toString());
+console.log(fruits.join(' - '));
+console.log(fruits, fruits.pop(), fruits);
+
+// Conditionals, Control flows, (if else)
+// 18-35 is my target demographic
+// && AND
+// || OR
+var age = 45;
+
+let student = {
+    first: 'Evan',
+    last: 'French',
+    age: '19',
+};
+if( (age >= 18) && (age <= 35) ) {
+    status = 'target demo';
+    console.log(status);
+}
+else{
+    status = 'not my audience';
+    console.log(status);
+}
+
+// Switch statements
+// differentiate between weekeday vs. weekend
+// day 0 is sunday
+// day 6 --> saturday -->
+// day 4 --> thursday --> weekday
+switch(2){
+    case 0:
+        text = 'weekend';
+        break;
+    case 5:
+        text = 'weekend';
+        break;
+    case 6:
+        text = 'weekend';
+        break;
+    default:
+        text = 'weekday';
+}
+console.log(text);
